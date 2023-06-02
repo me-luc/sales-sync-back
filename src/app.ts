e18080e0a32e7393bd6cb07021ced221261e06e4
me.luc <lukasalmeida372@gmail.com>
2023-06-01 21:03:10 -0300

    feat: prisma setup

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import routes from './routes';
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use('/', routes);
+app.use('/api/', routes);
 
 app.listen(3000, () => {
 	console.log('Server is running');
