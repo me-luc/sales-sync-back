30d0172939baf03cb7d1a05bd24a64e77a33d6b8
me.luc <lukasalmeida372@gmail.com>
2023-06-03 15:48:18 -0300

    feat: refactor to add utility functions

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/api/', routes);
 
-app.listen(3000, () => {
-	console.log('Server is running');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
 });
