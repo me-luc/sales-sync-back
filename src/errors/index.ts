9a3ff95ff639ddc63dcbf30e8a4bbd6fdf654105
me.luc <lukasalmeida372@gmail.com>
2023-06-03 23:34:43 -0300

    refactor: update import/export dynamics

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -2,3 +2,4 @@ export * from './bad-request-error';
 export * from './forbidden-error';
 export * from './invalid-credentials-error';
 export * from './unauthorized-error';
+export * from './not-found-error';
--- src/routes/health-route.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from 'express';
-
-const healthRoute = Router();
-
-healthRoute.get('/', (request: Request, response: Response) => {
-	return response.send({ message: 'Hello World!' });
-});
-
-export default healthRoute;
